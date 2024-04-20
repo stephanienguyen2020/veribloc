a5265449c8682343c306c3abcd1944b9afcd71ae
Stephanie Nguyen <tiennguyen.nltt@gmail.com>
2024-04-20 15:08:19 -0400

    Change

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Footer, Possibility, Features, Whatvb, Header } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
-import LoginForm from "./components/loginform/LoginForm";
+
 import "./App.css";
 
 const App = () => (
